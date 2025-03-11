18eb0eb4bf1d1ca3ce66654b7378da68b3fb423c
L-pixela <122270958+L-pixela@users.noreply.github.com>
2025-03-11 11:22:02 +0700

    Start Code

---
+++ lib/screen/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_demo/provider/count_notifier.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CounterModel>(
+      builder: (BuildContext context, counter, Widget? child) {
+        return Column(
+          children: [
+            Text("Count =  ${counter.count}"),
+            ElevatedButton(
+              onPressed: () => {counter.increment()},
+              child: Text("ADD 1"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
